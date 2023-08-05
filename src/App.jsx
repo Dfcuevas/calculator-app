8dd065b0ec47b33d553cdaab9f812fbccd15c7c1
Diego Cuevas <ingdcuevas@gmail.com>
2023-08-05 15:39:26 -0500

    Cambiando la logica, dividiendo las funciones del componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,79 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [screen, setScreen] = useState(0);
+  const [screen, setScreen] = useState("");
 
   const handleClick = (e) => {
-    setScreen(parseInt(e.target.innerText));
+    if (e.target.innerText === "RESET") {
+      setScreen("");
+    } else {
+      screen.length < 10
+        ? setScreen((prevState) => prevState + e.target.innerText)
+        : "";
+    }
   };
 
-  console.log(screen);
+  const handleOperatorsClick = (e) => {
+    console.log("You clicked me " + e.target.innerText);
+  };
+
   return (
     <main className="container-calc">
       <h1>calc</h1>
-      <section className="screen-section">{screen}</section>
+      <section className="screen-section">{!screen ? "0" : screen}</section>
       <section className="keys-section">
         <div className="primary-key" onClick={(e) => handleClick(e)}>
           7
         </div>
-        <div className="primary-key">8</div>
-        <div className="primary-key">9</div>
-        <div className="secondary-key">DEL</div>
-        <div className="primary-key">4</div>
-        <div className="primary-key">5</div>
-        <div className="primary-key">6</div>
-        <div className="primary-key">+</div>
-        <div className="primary-key">1</div>
-        <div className="primary-key">2</div>
-        <div className="primary-key">3</div>
-        <div className="primary-key">-</div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          8
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          9
+        </div>
+        <div className="secondary-key" onClick={(e) => handleOperatorsClick(e)}>
+          DEL
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          4
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          5
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          6
+        </div>
+        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+          +
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          1
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          2
+        </div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          3
+        </div>
+        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+          -
+        </div>
         <div className="primary-key">.</div>
-        <div className="primary-key">0</div>
-        <div className="primary-key">/</div>
-        <div className="primary-key">x</div>
-        <div className="secondary-key reset">RESET</div>
-        <div className="equal-key">=</div>
+        <div className="primary-key" onClick={(e) => handleClick(e)}>
+          0
+        </div>
+        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+          /
+        </div>
+        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+          *
+        </div>
+        <div className="secondary-key reset" onClick={(e) => handleClick(e)}>
+          RESET
+        </div>
+        <div className="equal-key" onClick={(e) => handleOperatorsClick(e)}>
+          =
+        </div>
       </section>
     </main>
   );
