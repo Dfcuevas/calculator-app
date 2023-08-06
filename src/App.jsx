777ea00c03cd5362971de118869546a1be0ab1b9
Diego Cuevas <ingdcuevas@gmail.com>
2023-08-05 20:01:48 -0500

    terminando la logica del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -4,75 +4,111 @@ import "./App.css";
 function App() {
   const [screen, setScreen] = useState("");
 
-  const handleClick = (e) => {
-    if (e.target.innerText === "RESET") {
-      setScreen("");
-    } else {
-      screen.length < 10
-        ? setScreen((prevState) => prevState + e.target.innerText)
-        : "";
-    }
-  };
-
-  const handleOperatorsClick = (e) => {
-    console.log("You clicked me " + e.target.innerText);
-  };
-
   return (
     <main className="container-calc">
       <h1>calc</h1>
       <section className="screen-section">{!screen ? "0" : screen}</section>
       <section className="keys-section">
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           7
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           8
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           9
         </div>
-        <div className="secondary-key" onClick={(e) => handleOperatorsClick(e)}>
+        <div
+          className="secondary-key"
+          onClick={(e) => setScreen(screen.slice(0, -1))}
+        >
           DEL
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           4
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           5
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           6
         </div>
-        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           +
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           1
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           2
         </div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           3
         </div>
-        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           -
         </div>
-        <div className="primary-key">.</div>
-        <div className="primary-key" onClick={(e) => handleClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
+          .
+        </div>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           0
         </div>
-        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           /
         </div>
-        <div className="primary-key" onClick={(e) => handleOperatorsClick(e)}>
+        <div
+          className="primary-key"
+          onClick={(e) => setScreen(screen + e.target.innerText)}
+        >
           *
         </div>
-        <div className="secondary-key reset" onClick={(e) => handleClick(e)}>
+        <div className="secondary-key reset" onClick={(e) => setScreen("")}>
           RESET
         </div>
-        <div className="equal-key" onClick={(e) => handleOperatorsClick(e)}>
+        <div className="equal-key" onClick={(e) => setScreen(eval(screen))}>
           =
         </div>
       </section>
